2e96348ec1393e4667dfb7dd7e389a9306cad310
abinaya0708 <aabinaya2001.com>
2023-11-07 10:41:10 +0530

    get

---
+++ get.js
@@ -0,0 +1,22 @@
+let today=new Date();
+console.log(today);
+
+
+
+let day=today.getDay();
+console.log(day);
+
+
+document.write("Find today is Weekend or not:---"+"<br>");
+
+if(day==6||day==7){
+	console.log("Today is Weekend");
+	
+	document.write("Today is Weekend"+"<br>");
+	
+}
+else{
+	console.log("Today is not Weekend");
+	
+	document.write("Today is not Weekend"+"<br>");
+}
